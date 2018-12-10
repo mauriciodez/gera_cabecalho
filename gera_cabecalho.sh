e62ea6fb5ca2f3d9c7150c5b651b5c1a5f79a80d
Maurício G. Paiva <mauricimgp@gmail.com>
2018-12-10 00:56:35 -0200

    Lançamento da versão 1.1 para correção de código

--- gera_cabecalho.sh
+++ gera_cabecalho.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #####	NOME:				gera_cabeçalho.sh
-#####	VERSÃO:				1.0
+#####	VERSÃO:				1.1
 #####	DESCRIÇÃO:			Insere cabeçalhos em novos scripts ou em existentes.
-#####	DATA DA CRIAÇÃO:	09/12/2018
+#####	DATA DA CRIAÇÃO:	10/12/2018
 #####	ESCRITO POR:		Maurício G. Paiva
 #####	E-MAIL:				mauriciodez@gmail.com
 #####	DISTRO:				Debian GNU/Linux 8 (jessie)
 #####	LICENÇA:			GPLv3
-#####	PROJETO:			https://github.com/mauriciodez/gera_cabeçalho
+#####	PROJETO:			https://github.com/mauriciodez/gera_cabeçalho.git
 
 TR="#####"
 USER="Maurício G. Paiva"
@@ -20,7 +20,7 @@ TXT2	()	{
 			sed -i "2s,^,\n$TR\tNOME:\t\t\t\t$(echo $ARQ)\n$TR\tVERSÃO:\t\t\t\t1.0\n$TR\tDESCRIÇÃO:\t\t\t$descricao \
 			\n$TR\tDATA DA CRIAÇÃO:\t$(date +"%d/%m/%Y")\n$TR\tESCRITO POR:\t\t$USER\n$TR\tE-MAIL:\t\t\t\t$EMAIL \
 			\n$TR\tDISTRO:\t\t\t\t$(cat /etc/*-release|head -1|awk -F'"' '{print $2}')\n$TR\tLICENÇA:\t\t\t$VS \
-			\n$TR\tPROJETO:\t\t\t$GIT$(echo $ARQ|cut -d'.' -f1)\n," $ARQ
+			\n$TR\tPROJETO:\t\t\t$GIT$(echo $ARQ|cut -d'.' -f1).git\n," $ARQ
 			}
 
 x="s"
@@ -32,7 +32,7 @@ clear && ls *.sh
 		read ARQ
 
 	if [ -z $ARQ ];then
-		echo "Mode de criação"
+		echo "Modo de criação"
 		read -p "Insira o nome para o novo arquivo -> " ARQ
 			if [ `echo $ARQ | cut -d'.' -f2` != "sh" ];then
 				ARQ=`echo "$ARQ.sh"`
@@ -41,8 +41,8 @@ clear && ls *.sh
 				read -p "Insira a descrição do script -> " descricao
 				TXT2
 				chmod +x $ARQ
-				IT=1
-	else
+				exit 0
+			else
 
 		if [ `echo $ARQ | cut -d'.' -f2` != "sh" ];then
 			ARQ=`echo "$ARQ.sh"`
@@ -52,10 +52,10 @@ clear && ls *.sh
 				read -p "Insira a descrição do script -> " descricao
 				TXT2
 			else
-				echo "Arquivo não encontardo quer fazer de novo ? ENTER ou 's'"
+				echo "Arquivo não encontardo quer fazer de novo ? [s]"
 				read resposta
 					if [ $resposta != $x ]; then
-						exit
+                  exit
 					fi
 			fi
 	fi
